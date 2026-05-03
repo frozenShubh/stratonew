1d54ab506551ef5bc01bcc71e1f8fd38fb2a261e
Shubham Agrawal <shubhamkalyanagrawal@gmail.com>
2026-05-03 22:07:15 +0530

    feat: implement router/helmet providers and add GitHub Actions deployment workflow for Vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 // Polyfill for Array.at used by some dependencies (like web-vitals) 
 // to support older browsers/crawlers used by react-snap
 if (!Array.prototype.at) {
-  Array.prototype.at = function(index) {
+  Array.prototype.at = function (index) {
     index = Math.trunc(index) || 0;
     if (index < 0) index += this.length;
     if (index < 0 || index >= this.length) return undefined;
@@ -10,23 +10,16 @@ if (!Array.prototype.at) {
   };
 }
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import "@/index.css";
 import App from "@/App";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  ReactDOM.hydrateRoot(rootElement, <App />);
 } else {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  root.render(<App />);
 }
